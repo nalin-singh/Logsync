9d361012410d20334307b823ef67169fe222005e
Nalin Singh <nalinsingh20@gmail.com>
2024-04-21 11:31:37 +0530

    Add : Contract PAge

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import { Application } from './pages';
 import { Error404 } from './pages/error404';
 import { Dashboard } from './pages/dashboard';
+import { Contract } from './pages/contract';
 
 export const appRouter = createBrowserRouter([
     {
@@ -11,8 +12,12 @@ export const appRouter = createBrowserRouter([
         errorElement: <Error404 />,
         children: [
             {
-                path: '/*',
+                path: '/',
                 element: <Dashboard />
+            },
+            {
+                path: '/',
+                element: <Contract />
             }
         ]
     }
